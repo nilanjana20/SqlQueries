2984775f7aca61e828ba87557cbed7dfda6bfd79
nilanjana20 <nilanjanac873@gmail.com>
2022-02-18 16:08:10 +0530

    [Nilanjana]Added employee payroll table

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,3 +1,10 @@
 ------------UC1 CREATE DATABASE---------------
 CREATE DATABASE payroll_service;
 USE payroll_service;
+------------CREATE TABLE UC2----------------
+CREATE TABLE employee_payroll (
+       Id int IDENTITY(1,1) PRIMARY KEY,
+	   Name varchar(200),
+	   Salary float,
+	   StartDate Date
+);
