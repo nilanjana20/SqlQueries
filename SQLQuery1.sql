e39fbc13dbc4e24d331854d5bd650546b89a6e2e
nilanjana20 <nilanjanac873@gmail.com>
2022-02-18 19:39:40 +0530

    [Nilanjana]Added sum avg min max count

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -15,12 +15,18 @@ values('Nilanjana',41000,'03-01-2022'),
 ('Altu',75000,'09-03-2020'),
 ('Neel',66000,'05-01-2021'),
 ('Biu',20000,'08-01-2021');
---------------------------UC4----------------------------
+--------------------------UC4-------------------------------
 select * from employee_payroll;
-------------------------UC5-----------------------------
+------------------------UC5----------------------------------
 select Salary from employee_payroll where name = 'Nilanjana';
 select Salary from employee_payroll where name = 'Biu';
-------------------------UC6------------------------------
+------------------------UC6----------------------------------
 ALTER TABLE employee_payroll ADD Gender char(1);
 update employee_payroll set Gender = 'F' where name = 'Nilanjana' or name= 'Shruti'or name = 'Biu';
-update employee_payroll set Gender = 'M' where name = 'Altu' or name= 'Altu';
+update employee_payroll set Gender = 'M' where name = 'Altu' or name= 'Neel';
+--------------------------UC7--------------------------------
+select sum(Salary) from employee_payroll where gender = 'M' group by Gender;
+select avg(Salary) from employee_payroll where gender = 'F' group by Gender;
+select min(Salary) from employee_payroll where gender = 'M' group by Gender;
+select max(Salary) from employee_payroll;
+select count(Salary) from employee_payroll ;
